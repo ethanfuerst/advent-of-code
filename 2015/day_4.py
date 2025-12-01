dfc8aa96e7916885a0cfece733fca6c50e3437c9
Ethan Fuerst <34084535+ethanfuerst@users.noreply.github.com>
2025-11-30 19:50:44 -0500

    day 4 2015

---
+++ 2015/day_4.py
@@ -0,0 +1,22 @@
+import hashlib
+
+i = 'yzbqklnj'
+
+def check_starts_with_zeros(num_zeros):
+    starts_with_zeros = False
+    start_number = 1
+
+    while not starts_with_zeros:
+        new_str = f'{i}{start_number}'
+        res = hashlib.md5(new_str.encode()).hexdigest()
+
+        str_match = '0' * num_zeros
+        if res[:num_zeros] == str_match:
+            starts_with_zeros = True
+
+        start_number += 1
+
+    return start_number
+
+print(f'Lowest number that hash starts with 5 zeros: {check_starts_with_zeros(5)}')
+print(f'Lowest number that hash starts with 6 zeros: {check_starts_with_zeros(6)}')

7981b48a2b79bba069c546d4f572a62d2ab039dc
Ethan Fuerst <34084535+ethanfuerst@users.noreply.github.com>
2025-12-05 12:07:01 -0500

    2025 day 3 part 2 partial

--- 2025/day_3.py
+++ 2025/day_3.py
@@ -1,4 +1,6 @@
 # https://adventofcode.com/2025/day/3
+from itertools import combinations
+import time
 
 with open('2025/inputs/day_3.txt', 'r') as file:
     data = file.read().splitlines()
@@ -16,5 +18,22 @@ for line in data:
                 line_max = to_check
         lag += 1
     total += line_max
-    
+
+print(total)
+
+# works in test case but too expensive to get correct answer with all data
+s = int(time.time())
+total = 0
+count = 0
+for line in data:
+    max = 0
+    for i in combinations(line, 12):
+        m = int(''.join(i))
+        if m > max:
+            max = m
+    total += max
+    count += 1
+    nt = int(time.time())
+    print(nt - s, count, max)
+
 print(total)
